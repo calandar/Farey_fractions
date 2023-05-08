cc0a8c8809281f4b3a04cc3fe168eff63da15139
Кирилл Ананьянц <ankir02@gmail.com>
2023-05-09 00:51:02 +0300

    this shit works omfg!!!1!11

--- farey/Farey_core.cpp
+++ farey/Farey_core.cpp
@@ -1,60 +1,63 @@
 #include "Farey_core.h"
 
+
 /* TODO:
         1. Проверка на деление на 0
 */
 
-bool no_overflow(int64_t a, int64_t b) {  // header??!
-    int64_t sum = a + b;
-    int64_t ssd = a ^ b;
-    if ((ssd >= 0) && ((ssd ^ sum) < 0))
-        return false; // если переполнение - false
-    return true;
-}
-
-int64_t safe_mul(int64_t x, int64_t y, int64_t m) { // в header?!
-    if (x > y) {
-        int64_t tmp = x;
-        x = y;
-        y = tmp;
-    }
-    int64_t res = 0;
-    int64_t iy = y;
-    while (x) {
-        if (x & 1) {
-            if (!no_overflow(res, iy)) res -= m;
-            res = (res + iy) % m;
-        }
-        int64_t new_iy = iy;
-        if (!no_overflow(new_iy, iy)) new_iy -= m;
-        iy = (new_iy + iy) % m;
-        x >>= 1;
-    }
-    return  res;
-}
 
 
-int64_t bin_pow(int64_t a, int64_t n, int64_t mod) { // бинарное возведение в степень
-    int64_t res = 1;
-    while (n) {
-        if (n % 2 == 1) res = ((res % mod) * a) % mod;
-        a = a * a % mod;
-        n >>= 1; // n = n / 2;
-    }
-    return res;
-}
+//bool no_overflow(BigInt a, BigInt b) {  // header??!
+//    BigInt sum = a + b;
+//    BigInt ssd = a ^ b;
+//    if ((ssd >= 0) && ((ssd ^ sum) < 0))
+//        return false; // если переполнение - false
+//    return true;
+//}
 
-//int64_t inverse_modulo(int64_t num, int64_t mod) { // используем малую теорему Ферма для нахождения обратного числа
+//BigInt safe_mul(BigInt x, BigInt y, BigInt m) { // в header?!
+//    if (x > y) {
+//        BigInt tmp = x;
+//        x = y;
+//        y = tmp;
+//    }
+//    BigInt res = 0;
+//    BigInt iy = y;
+//    while (x) {
+//        if (x & 1) {
+//            if (!no_overflow(res, iy)) res -= m;
+//            res = (res + iy) % m;
+//        }
+//        BigInt new_iy = iy;
+//        if (!no_overflow(new_iy, iy)) new_iy -= m;
+//        iy = (new_iy + iy) % m;
+//        x >>= 1;
+//    }
+//    return  res;
+//}
+
+
+//BigInt bin_pow(BigInt a, BigInt n, BigInt mod) { // бинарное возведение в степень
+//    BigInt res = 1;
+//    while (n) {
+//        if (n % 2 == 1) res = ((res % mod) * a) % mod;
+//        a = a * a % mod;
+//        n >>= 1; // n = n / 2;
+//    }
+//    return res;
+//}
+
+//BigInt inverse_modulo(BigInt num, BigInt mod) { // используем малую теорему Ферма для нахождения обратного числа
 //    return bin_pow(num, mod - 2, mod); // !!!! int?
 //}
 
-int64_t inverse_modulo(int64_t num, int64_t mod) {
-    int64_t M[2][2];
+BigInt inverse_modulo(BigInt num, BigInt mod) {
+    BigInt M[2][2];
     M[0][0] = mod;
     M[1][0] = num;
     M[0][1] = 0;
     M[1][1] = 1;
-    int64_t r = 0, q = 0;
+    BigInt r = 0, q = 0;
     while (M[1][0] != 1) {
         r = M[0][0] % M[1][0];
         q = M[0][1] - M[0][0] / M[1][0] * M[1][1];
@@ -69,12 +72,12 @@ int64_t inverse_modulo(int64_t num, int64_t mod) {
 }
 
 Farey_fraction::Farey_fraction() {}
-Farey_fraction::Farey_fraction(int64_t m, int64_t n, int64_t number) {
+Farey_fraction::Farey_fraction(BigInt m, BigInt n, BigInt number) {
     mod = m;
     N = n;
     num = number;
 }
-Farey_fraction::Farey_fraction(int64_t m, int64_t n, int64_t num, int64_t denom) {
+Farey_fraction::Farey_fraction(BigInt m, BigInt n, BigInt num, BigInt denom) {
     mod = m;
     N = n;
     numerator = num;
@@ -111,7 +114,7 @@ Farey_fraction::Farey_fraction(int64_t m, int64_t n, int64_t num, int64_t denom)
 //}*/  //старые операторы
 
 Farey_fraction& Farey_fraction::operator + (const Farey_fraction& rhs) {
-    if (!no_overflow(num, rhs.get_num())) num -= mod; // аккуратно с переполнением
+   // if (!no_overflow(num, rhs.get_num())) num -= mod; // аккуратно с переполнением
     this->num = (num + rhs.get_num()) % mod;
     Normalize();
     reverse_calc();
@@ -126,25 +129,27 @@ Farey_fraction& Farey_fraction::operator - (const Farey_fraction& rhs) {
 }
 
 Farey_fraction& Farey_fraction::operator * (const Farey_fraction& rhs) {
-    this->num = safe_mul(num, rhs.get_num(), mod);
+  //  this->num = safe_mul(num, rhs.get_num(), mod);
+    this->num = (num * rhs.get_num()) % mod;
     Normalize();
     reverse_calc();
     return *this;
 }
 Farey_fraction& Farey_fraction::operator / (const Farey_fraction& rhs) {
-    this->num = safe_mul(num, swapped(rhs).get_num(), mod);
+    //this->num = safe_mul(num, swapped(rhs).get_num(), mod);
+    this->num = (num * swapped(rhs).get_num()) % mod;
     Normalize();
     reverse_calc();
     return *this;
 }
 
-int64_t Farey_fraction::get_numerator() const {
+BigInt Farey_fraction::get_numerator() const {
     return numerator;
 }
-int64_t Farey_fraction::get_denominator() const {
+BigInt Farey_fraction::get_denominator() const {
     return denominator;
 }
-int64_t Farey_fraction::get_num() const {
+BigInt Farey_fraction::get_num() const {
     return num;
 }
 
@@ -162,18 +167,19 @@ void Farey_fraction::calc() {
     }
     inv %= mod;
     //num = (numerator * inv) % mod;  // ОПАСНО: ПЕРЕПОЛНЕНИЕ :cccccccccccccccccccccccccccc
-    num = safe_mul(numerator, inv, mod);
+   // num = safe_mul(numerator, inv, mod);
+    num = (numerator * inv) % mod;
 }
 
 void Farey_fraction::reverse_calc() {
-    int64_t M[2][2];
-    M[0][0] = static_cast<int64_t>(mod);
+    BigInt M[2][2];
+    M[0][0] = static_cast<BigInt>(mod);
     M[0][1] = 0;
     M[1][0] = num;
     M[1][1] = 1;
-    int64_t r = 0, q = 0;
+    BigInt r = 0, q = 0;
 
-    while (abs(M[1][0] > (int64_t)N) || abs(M[1][1] > (int64_t)N)) { // 
+    while (abs(M[1][0] > (BigInt)N) || abs(M[1][1] > (BigInt)N)) { // 
         r = M[0][0] % M[1][0];
         q = M[0][1] - (M[0][0] / M[1][0]) * M[1][1];
         M[0][0] = M[1][0];
@@ -184,8 +190,8 @@ void Farey_fraction::reverse_calc() {
             M[1][0] = mod;
         }
     }
-    numerator = static_cast<int64_t>(M[1][0] * (M[1][1] / abs(M[1][1])));
-    denominator = static_cast<int64_t>(abs(M[1][1]));
+    numerator = static_cast<BigInt>(M[1][0] * (M[1][1] / abs(M[1][1])));
+    denominator = static_cast<BigInt>(abs(M[1][1]));
     if (numerator == 0) {
         denominator = 1;
     }
--- farey/Farey_core.h
+++ farey/Farey_core.h
@@ -1,29 +1,31 @@
 #pragma once
-#include <cstdint>
+//#include <cstdint>
 #include <iostream>
+#include "BigInt.h"
 
-int64_t inverse_modulo(int64_t num, int64_t mod); // используем малую теорему Ферма для нахождения обратного числа
-int64_t bin_pow(int64_t a, uint64_t n, uint64_t mod); // бинарное возведение в степень
+//BigInt inverse_modulo(BigInt num, BigInt mod); // используем малую теорему Ферма для нахождения обратного числа
+//BigInt bin_pow(BigInt a, uint64_t n, uint64_t mod); // бинарное возведение в степень
+
 
 
 class Farey_fraction {
 public:
 	Farey_fraction();
-	Farey_fraction(int64_t m, int64_t n, int64_t number);
-	Farey_fraction(int64_t m, int64_t n, int64_t num, int64_t denom);
+	Farey_fraction(BigInt m, BigInt n, BigInt number);
+	Farey_fraction(BigInt m, BigInt n, BigInt num, BigInt denom);
 	Farey_fraction& operator + (const Farey_fraction& rhs);
 	Farey_fraction& operator - (const Farey_fraction& rhs);
 	Farey_fraction& operator * (const Farey_fraction& rhs);
 	Farey_fraction& operator / (const Farey_fraction& rhs);
-	int64_t get_numerator() const;
-	int64_t get_denominator() const;
-	int64_t get_num() const;
+	BigInt get_numerator() const;
+	BigInt get_denominator() const;
+	BigInt get_num() const;
 private:
-	int64_t numerator;  // Числитель дроби Фарея
-	int64_t denominator; // Знаменитель дроби Фарея
-	int64_t mod; // Модуль
-	int64_t N; // Порядок дроби Фарея
-	int64_t num; // Число в модулярной арифметике, соответствующее дроби Фарея
+	BigInt numerator;  // Числитель дроби Фарея
+	BigInt denominator; // Знаменитель дроби Фарея
+	BigInt mod; // Модуль
+	BigInt N; // Порядок дроби Фарея
+	BigInt num; // Число в модулярной арифметике, соответствующее дроби Фарея
 	void calc();
 	void Normalize();
 	void reverse_calc();   // найти дробь по num с помощью расширенного алгоритма Евклида
